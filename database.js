d3d012ca69ee7ac31eb7646704ce9e915794c97b
Tamer-ElSayed <tamerelbary123@gmail.com>
2024-03-17 17:42:25 +0200

    fixed table loading error

--- database.js
+++ database.js
@@ -93,11 +93,51 @@ async function importJSONFromFileToDb() {
 async function main() {
   if (shouldRunFunction()) {
     // Call the function to import JSON data from the file to db
-    importJSONFromFileToDb();
-    
+    await importJSONFromFileToDb();
   }
+  writeTable();
 };
 
 
 main();
 
+
+
+async function writeTable() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Open the IndexedDB database
+      const request = indexedDB.open('SMRF-Database', 1);
+
+      request.onerror = function (event) {
+        console.error("Database error: " + event.target.errorCode);
+      };
+
+      request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction('defaultPackage', 'readonly');
+        const objectStore = transaction.objectStore('defaultPackage');
+        const dataTable = document.getElementById('defaultListTable').getElementsByTagName('tbody')[0];
+
+        objectStore.openCursor().onsuccess = function (event) {
+          const cursor = event.target.result;
+          if (cursor) {
+            const row = dataTable.insertRow();
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+            cell1.textContent = cursor.key;
+            cell2.textContent = cursor.value.Enzyme;
+            cell3.textContent = cursor.value.RestrictionSite;
+            cursor.continue();
+          }
+        };
+      };
+
+
+    } catch (error) {
+      console.error('Error:', error);
+      reject(error);
+    }
+  });
+}
